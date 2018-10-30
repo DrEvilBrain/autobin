64f0fa15051c4ad3b6b9064b22ab0067bb13ae9d
Unknown <drevilbrain@gmail.com>
2018-10-30 15:41:23 -0400

    Add edge cases
    - Empty URL
    - URL is not a string
    - Status code != 200

--- wasteZero/main.py
+++ wasteZero/main.py
@@ -1,22 +1,34 @@
 from api import apiCall
 from sortImage import sortImage
-		
-# url to do image recognition on (input as string)
+
+# image url to do image recognition on (input as string)
 url = "https://upload.wikimedia.org/wikipedia/commons/e/eb/Box.agr.jpg"
 
-# makes apiCall and fetches formated data from it
-status_code, json, tags, caption, confidence = apiCall(url)
- 
-# output the response status code. 200 is the correct response
-print("Status Code: ", status_code)
+# checks if the image url is a valid string
+if(url == None or url == ""):
+	print("URL is empty")
+elif not(isinstance(url, str)):
+	print("URL is not a string")
+else:
+	# makes apiCall and fetches formated data from it
+	status_code, json, tags, caption, confidence = apiCall(url)
 
-# output the image's tags and keywords
-print("Tags: ", tags)
+	# checks if API response status code is 200 (valid response)
+	if(status_code == 200):
+		# output the response status code. 200 is the correct response
+		print("Status Code: ", status_code)
 
-# output a phrase to describe/caption the image
-print("Captions: ", caption)
+		# output the image's tags and keywords
+		print("Tags: ", tags)
 
-# output a number describing how accurate the phase is
-print("Confidence: ", confidence)
+		# output a phrase to describe/caption the image
+		print("Captions: ", caption)
 
-sortImage(tags)
+		# output a number describing how accurate the phase is
+		print("Confidence: ", confidence)
+
+		# sort image by the image's tags given from the API
+		sortImage(tags)
+	else:
+		# output response status code for debugging purposes
+		print("Response Status Code: ", status_code)
